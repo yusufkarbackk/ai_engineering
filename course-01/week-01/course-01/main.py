b556bf50930e358ffa6a36a93f4dc2dc75c50824
yusufkarbackk <yusufkarback64@gmail.com>
2026-09-13 21:25:55 +0700

    fix week-01 course-01

--- course-01/week-01/course-01/main.py
+++ course-01/week-01/course-01/main.py
@@ -1,3 +1,24 @@
+usage = {
+    "model": "gpt-5",
+    "provider": "openai",
+    "input_tokens": 1000,
+    "output_tokens": 500,
+    "latency": 1.2,
+}
+
+def calculate_total_tokens(input_tokens, output_tokens):
+    return input_tokens + output_tokens
+
+def calculate_cost(input_tokens, output_tokens):
+        input_token_cost = 0.000001
+        output_token_cost = 0.000002
+
+        input_cost = input_token_cost * input_tokens
+        output_cost = output_token_cost * output_tokens
+
+        total_cost = input_cost + output_cost
+        return total_cost
+
 class LLMUsage:
     def __init__(self, model, provider, input_tokens, output_tokens, latency):
         self.model = model
@@ -6,8 +27,8 @@ class LLMUsage:
         self.output_tokens = output_tokens
         self.latency = latency
 
-    def calculate_total_token(self):
-        return self.input_tokens + self.output_tokens
+    def calculate_total_tokens(self):
+        return calculate_total_tokens(self.input_tokens, self.output_tokens)
 
     def calculate_cost(self):
         input_token_cost = 0.000001
@@ -19,20 +40,29 @@ class LLMUsage:
         total_cost = input_cost + output_cost
         return total_cost
     
+class LLMProvider:
+    def generate(self):
+        return "Generating text using the LLM provider."
     
-LLM1 = LLMUsage("gpt-5", "openai", 1000, 500, 1.2)
-LLM2 = LLMUsage("gpt-4", "openai", 2000, 1000, 1.5)
+class OpenAIProvider(LLMProvider):
+    def generate(self):
+        return "Generating text using OpenAI's LLM."
+class AnthropicProvider(LLMProvider):
+    def generate(self):
+        return "Generating text using Anthropic's LLM."
+    
+    
+llm1 = LLMUsage("gpt-5", "openai", 1000, 500, 1.2)
+llm2 = LLMUsage("gpt-4", "openai", 2000, 1000, 1.5)
 
-print(f"LLM1 Total Tokens: {LLM1.calculate_total_token()}")
-print(f"LLM1 Cost: ${LLM1.calculate_cost():.6f}")
-print(f"LLM2 Total Tokens: {LLM2.calculate_total_token()}")
-print(f"LLM2 Cost: ${LLM2.calculate_cost():.6f}")
+print(f"llm1 Total Tokens: {llm1.calculate_total_tokens()}")
+print(f"llm1 Cost: ${llm1.calculate_cost():.6f}")
+print(f"llm2 Total Tokens: {llm2.calculate_total_tokens()}")
+print(f"llm2 Cost: ${llm2.calculate_cost():.6f}")
 
+openai_provider = OpenAIProvider()
+anthropic_provider = AnthropicProvider()
 
-# usage = {
-#     "model": "gpt-5",
-#     "provider": "openai",
-#     "input_tokens": 1000,
-#     "output_tokens": 500,
-#     "latency": 1.2,
-# }
+print(openai_provider.generate())
+print(anthropic_provider.generate())
+
